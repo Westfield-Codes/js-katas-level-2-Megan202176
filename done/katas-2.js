c5390263b4534888cf9f39010ee705ecd287adf2
Megan202176 <megan202176@student-schoolsofwestfield.org>
2025-10-22 16:08:26 +0000

    rand student

--- done/katas-2.js
+++ done/katas-2.js
@@ -1,11 +1,11 @@
 /* ICE CREAM CONE KATA */
 
 /* Var, Alert, Prompt, Conditional, While, Function. Global, Concatenation */
+
 /* Initialize Globals
  * Set a global variable myScoops = "Here's your cone with: "
  */
-var myScoops = "Here's your cone with: ";
-
+var myScoops="Here's your cone with: "
 /* FUNCTION main() 
  * initialize variable scoops, prompt user for "how many scoops (max 3)?"
  * if scoops is less than 4, call scoopLoop with argument scoops
@@ -14,13 +14,13 @@ var myScoops = "Here's your cone with: ";
  * @return: none (main never gets returns
  */
 function main(){
-    let scoops=prompt("how many scoops (max 3)?");
-    if(scoops<4) scoopLoop(scoops);
-    else{
-        alert("oink oink");
-        main()
+    let scoops= prompt("how many scoops (max 3)?")
+    if (scoops<4) scoopLoop(scoops)
+    else {
+    alert("oink oink") 
+    main()
     }
-    }
+}
 /* FUNCTION scoopLoop(scoops) 
  * start scoop at 1 and loop while scoop is less than or equal to scoops
  * inside the loop add nextScoop(scoop) to myScoops
@@ -35,5 +35,15 @@ function scoopLoop(scoops){
         scoop++;
     }
     alert (myScoops);
-    myScoops = "Here's your cone with: ";
+    myScoops="Here's your cone with: "
+}
+/* FUNCTION nextScoop(scoop)
+ * initialize a variable called flavor, prompt for "Flavor for scoop number [scoop]"
+ * @param: scoop (integer)
+ * @return: flavor (string)
+ */
+function nextScoop(scoop){
+    let flavor=prompt("Flavor for scoop number " + scoop)
+    scoop++
+    return flavor
 }
