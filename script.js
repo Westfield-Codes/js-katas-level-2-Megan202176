70ecf781bbad87497b2b7b76290c5c0621c1229d
Megan202176 <megan202176@student-schoolsofwestfield.org>
2025-11-06 17:13:53 +0000

    changes

--- script.js
+++ script.js
@@ -1,66 +1,55 @@
-/* FUNCTION main() 
- * Perform basic arithmetic as a calculator
- * @param: none (main never gets params)
- * @return: none (main never gets returns)
- */
- // Ask the user what operation they want (+, -, *, /)
- // Ask the user for the first number (a)
- // Parse a as an integer
- // Ask the user for the second number (b)
- // Parse b as an integer
- // Based on the operation, send the numbers to the right function as arguments 
- // store the returned value in variable called result
- // Alert the equation a + operation + b = result
+/* SHOPPING LIST KATA:  */
+
+// Every day, you are sent to buy 5 items. 
+
+/* 1. for loop and string concatenation */
 function main(){
-    let operation=prompt("What do you want? + - * /")
-    let a = prompt("What will A value?")
-    let b = prompt("What will B value?")
-    b=parseInt(b)
-    a=parseInt(a)
-
-        if (operation=="+"){
-            result=add(a,b)}
-        else if (operation=="-")result=subtract(a,b)
-        else if (operation=="*")result=mult(a,b)
-        else(opertation=="/"),result=div(a,b)
-
-    alert(a + operation + b +" = "+ result)
+    makeList(2)
 }
-
-
-/* FUNCTION add(a,b)
- * define a function called add with two number parameters a and b
- * @param: a, b (integers)
- * @return: a + b
+/* Function makeList()
+ * Create a shopping list string called shoppingList as "To Buy:"
+ * create a five-item loop, and in the loop, 
+ * ask for next item to buy (item #), and add it to the list 
+ * add a line break after each item added
+ * when loop's done, alert the list
  */
-function add(a,b){
-return a+b
-}
-/* FUNCTION subtract(a,b)
- * define a function called subtract with two number parameters a and b
- * @param: a, b (integers)
- * @return: a - b
+
+/* 2. for loop and array with toString */
+
+/* Same function, using previous code, BUT:
+ * build shopArray variable for the shopping list
+ * adjust loop to work with the array index values (start at 0)
+ * ask for next list item and push it to the shopArray
+ * no need to add a line break after
+ * at end of loop, alert the list title, followed by
+ * the list with toString() method (it won't have line breaks)*/
+function makeList(items){
+    let shopArray=[]
+    for(let i=0; i<=items; i++){
+        shopArray.push(prompt("Item: " + i + " ?"))
+    }
+    alert ("shopping list" + shopArray.toString())
+} 
+
+/* 3. for loop and array with join method 
+
+/* Same function, using previous code, BUT:
+ * the at end, alert list with array.join() method with line breaks 
+ * instead of commas */
+ 
+/* 4. 2D Arrays: add Pricing */
+
+/* Same function, using previous code, but THIS TIME add prices
+ * When someone enters an item, prompt them also to enter a price separating by comma (like milk, 1.99)])   
+ * Convert that entry to an itemPrice array by using string.split() and use a comma as delimiter
+ * Push the itemPrice array to shopArray
  */
-function subtract(a,b){
-return a-b
-    
-}
-/* FUNCTION multiply(a,b)
- * define a function called multiply with two number parameters a and b
- * @param: a, b (integers)
- * @return: a * b (* means multiply)
+
+/* 5. Total up the prices */
+
+/* Same function, using previous code, but THIS TIME total the prices.
+ * After you have built the array, create a NEW loop that adds the price 
+ * of each item to a total. Do this before the alert. 
+ * Save the first alert content as message. 
+ * Add total as a line below message, then alert the message. 
  */
-function mult(a,b){
-return a*b
-    
-}
-/* FUNCTION divide(a,b)
- * define a function called divide with two number parameters a and b
- * @param: a, b (integers)
- * @return: a / b (/ means divided by)
- */
-function div(a,b){
-return a/b
-    
-}
-/* ************************************* */
